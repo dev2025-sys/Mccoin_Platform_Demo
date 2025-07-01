cf4cb10626ae750f9cae76c4e12bc94182540f0c
Hassan Jarko <hassan.jarko@yahoo.com>
2025-07-01 15:39:39 +0400

    fixed

--- src/app/[locale]/news/page.tsx
+++ src/app/[locale]/news/page.tsx
@@ -1,11 +1,27 @@
-import NewsPage from "@/pages/NewsPage";
+import BusinessNews from "@/components/news-page/BusinessNews";
+import DontMiss from "@/components/news-page/DontMiss";
+import FintechNews from "@/components/news-page/FintechNews";
+import RecentPosts from "@/components/news-page/RecentPosts";
+import { TopNewsHero } from "@/components/news-page/TopNewsHero";
+import { TopNewsSlider } from "@/components/news-page/TopNewsSlider";
+import Footer from "@/components/shared/Footer";
+import Navbar from "@/components/shared/Navbar";
 
-const page = () => {
+const NewsPage = () => {
   return (
-    <section>
-      <NewsPage />
+    <section className="bg-[#07153b]">
+      <div className="mb-6">
+        <Navbar />
+      </div>
+      <TopNewsHero />
+      <TopNewsSlider />
+      <FintechNews />
+      <BusinessNews />
+      <RecentPosts />
+      <DontMiss />
+      <Footer />
     </section>
   );
 };
 
-export default page;
+export default NewsPage;
